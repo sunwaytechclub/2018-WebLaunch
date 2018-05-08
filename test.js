43885dc14dbc49c83ca11268341c60101ab4650d
potateros <ericngu97@gmail.com>
2018-05-08 12:44:13 +0800

    added skeletons

---
+++ test.js
@@ -0,0 +1,56 @@
+  // getElementById
+
+
+  // getElementById
+
+
+  //getElementByTagName
+
+
+  //createElement
+
+
+  //getElementStyle
+
+
+  //changeElementStyle
+
+
+  //element.style.property
+
+
+  //appendChild
+
+
+  //removeChild
+
+
+  // This is a JavaScript object
+  var fruits = {
+    0:{
+      "name": "Apple",
+      "price": 50,
+      "weight": 125
+    },
+    1:{
+      "name": "Orange",
+      "price": 60,
+      "weight": 230
+    },
+    2:{
+      "name": "Pear",
+      "price": 35,
+      "weight": 200
+    },
+    3:{
+      "name": "Durian",
+      "price": 120,
+      "weight": 500
+    }
+  }
+
+
+  //print loop
+
+
+  //clearing list
